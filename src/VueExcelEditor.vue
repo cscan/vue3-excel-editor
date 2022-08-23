4c992968b3c3ebc9eb5a4835b07f200f35f8a4b3
Kenneth Cheng <kenneth.cheng@ortholite.hk>
2022-08-23 18:46:55 +0800

    fix moveSouth bug if new-if-bottom

--- src/VueExcelEditor.vue
+++ src/VueExcelEditor.vue
@@ -1043,7 +1043,8 @@ export default {
       if (!count) return
 
       const fullWidth = this.editor.getBoundingClientRect().width
-      const viewWidth = this.fields.filter(f => !f.invisible).reduce((c, f) => c - -f.width.replace(/px$/, ''), 0) + 40
+      const viewWidth = this.fields.filter(f => !f.invisible).reduce((c, f) => c - -f.width.replace(/px$/, ''), 0)
+        + (this.noNumCol ? 0 : 40)
       const fillWidth = viewWidth - fullWidth + 2
       if (fillWidth)
         doFields.forEach(f => f.minWidth = (f.width.replace(/px$/, '') - fillWidth / count) + 'px')
@@ -2186,7 +2187,8 @@ export default {
     },
     moveSouth () {
       if (this.focused) {
-        if (this.currentRowPos + 1 >= this.table.length) {
+        // if (this.currentRowPos + 1 >= this.table.length) {
+        if (this.currentRowPos+1 >= (this.pageBottom - this.pageTop) && this.pageBottom >= this.table.length) {
           if (this.readonly) return false
           if (!this.newIfBottom) return false
           this.newRecord({}, false, true)
